215bd3d71573d197c574e4ebcebe4e80828afa8c
amoses-55 <amoses@springofrenewal.com>
2026-07-21 08:54:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-function showMessage(message) {
-    const messageBox = document.getElementById("message");
-    messageBox.textContent = message;
-    messageBox.scrollIntoView({ behavior: "smooth" });
-}
 const sessionSteps = [
     {
         title: "Opening Script",
